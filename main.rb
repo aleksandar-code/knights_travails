f24d26632ca5bb3624c14b7e7660eb5ec5bdb039
aleksandar-code <aca.jevdjevic@gmail.com>
2022-07-24 16:51:54 +0200

    Add board

--- main.rb
+++ main.rb
@@ -5,19 +5,37 @@
 
 class Board
   def initialize
-    @board = Array.new(8) { [*1..8] }
+    @board = [
+
+      ["00", "01", "02", "03", "04", "05", "06", "07"],
+      ["10", "11", "12", "13", "14", "15", "16", "17"],
+      ["20", "21", "22", "23", "24", "25", "26", "27"],
+      ["30", "31", "32", "33", "34", "35", "36", "37"],
+      ["40", "41", "42", "43", "44", "45", "46", "47"],
+      ["50", "51", "52", "53", "54", "55", "56", "57"],
+      ["60", "61", "62", "63", "64", "65", "66", "67"],
+      ["70", "71", "72", "73", "74", "75", "76", "77"],
+
+    ]
 		@knight = Knight.new(@board)
   end
   attr_accessor :board
   
   def show_square(row, col)
-    row -= 1
-    col -= 1
+    
     p @board[row][col]
   end
 
-  def knight_moves(current_location, destination)
+  def knight_moves(starting_square, destination)
+    
 
+  end
+
+  def display_board
+    board = @board
+    board.each_with_index do |row, idx|
+      print "#{row}\n"
+    end
   end
 end
 
@@ -29,4 +47,8 @@ class Knight
 
 end
 board = Board.new()
-board.show_square(8, 8)
+board.show_square(7, 7)
+
+board.display_board
+
+board.knight_moves([0,0], [1,2])
