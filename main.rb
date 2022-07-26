2de8440e22b8fc34d236c52d8ec391c331cdd835
aleksandar-code <aca.jevdjevic@gmail.com>
2022-07-26 12:30:06 +0200

    Add pseudocode

--- main.rb
+++ main.rb
@@ -3,32 +3,27 @@
 # 4,4 then lower is 2 down 4,2 and 1 down 3,2 for the left for the right
 # it's up and not down. next level is 1 down two up or down for either sides, upper levels are same
 
+
+
 class Board
+
   def initialize
-    @board = [
-
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-      [0, 1, 2, 3, 4, 5, 6, 7],
-
-    ]
+    @board = Array.new(8) {[*0..7]}
 		@knight = Knight.new(@board)
+    @root = nil
   end
   attr_accessor :board
   
   def show_square(row, col)
-    
     p @board[row][col]
   end
 
+ 
   def knight_moves(starting_square, destination_square)
     
-
+# get position of knight then build a tree to destination square and print the edges.
+# tree printed so start square 0,0 and all possible moves from that tree then the next tree then the next tree
+# until all of the squares are visited then find the shortest path, using BFS and DFS to find the destination
   end
 
   def display_board
@@ -37,6 +32,9 @@ class Board
       print "row #{idx}: #{row}\n"
     end
   end
+
+  
+
 end
 
 class Knight
@@ -47,7 +45,6 @@ class Knight
 
 end
 board = Board.new()
-board.show_square(7, 7)
 
 board.display_board
 
