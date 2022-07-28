afe71fff39562f18eeb91478181a4c6b12799396
aleksandar-code <aca.jevdjevic@gmail.com>
2022-07-28 13:04:43 +0200

    Create a lis of all the possible moves in the tree

--- main.rb
+++ main.rb
@@ -1,13 +1,64 @@
 # frozen_string_literal: true
 
+
+require 'pry-byebug'
 # 4,4 then lower is 2 down 4,2 and 1 down 3,2 for the left for the right
 # it's up and not down. next level is 1 down two up or down for either sides, upper levels are same
+
+class Node
+  def initialize(value = nil, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+  attr_accessor :value, :left, :right
+end
+
+class Tree
+  def initialize(array = [])
+    @array = array
+    @root = nil
+  end
+
+  def root(root)
+    @root = root
+  end
+
+  def array(array)
+    @array = array
+  end
+
+  def build_tree(array = nil, start_arr = 0, end_arr = 0, count = 0)
+    
+    return nil if start_arr > end_arr 
+    array = @array.uniq if count == 0 && array.nil?
+    count += 1
+    start_arr = 0
+    end_arr = array.length - 1
+    mid_arr = (start_arr + end_arr) / 2
+    root = Node.new(array[mid_arr])
+    root.left = build_tree(array[start_arr.. mid_arr-1], start_arr, mid_arr-1, count)
+    root.right = build_tree(array[mid_arr+1.. end_arr], mid_arr+1, end_arr, count)
+    root(root)
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  
+end
+
 class Board
   def initialize
     @board = Array.new(8) { [*0..7] }
-    @root = nil
+    @array = []
+    @knight = nil
+    @trees = []
   end
-  attr_accessor :board
+  attr_accessor :board, :array, :trees
 
   def show_square(row, col)
     p @board[row][col]
@@ -18,8 +69,28 @@ class Board
   # tree printed so start square 0,0 and all possible moves from that tree then the next tree then the next tree
   # until all of the squares are visited then find the shortest path, using BFS and DFS to find the destination
   def knight_moves(starting_square, destination_square)
-    Knight.new(self, starting_square)
+    @knight = Knight.new(self, starting_square, board)
+    @knight.rec(starting_square[0], starting_square[1])
+    i = 0
+    until (@array[i][0] ==7 && @array[i][1] == 7) do
 
+      # binding.pry
+      for moves in @array 
+        if moves == destination_square
+          puts "#{starting_square}" 
+          puts "#{destination_square}" 
+          return 
+        end
+      end
+      my_tree = Tree.new(@array)
+      my_tree.build_tree
+      my_tree.pretty_print
+      @knight.rec(@array[i][0], @array[i][1])
+      i += 1
+
+    end
+
+    
   end
 
   def display_board
@@ -28,46 +99,70 @@ class Board
       print "row #{idx}: #{row}\n"
     end
   end
+
+  def possible_moves(row, col)
+    # binding.pry
+    @array << [row, col]
+  end
 end
 
 # Knight has to return the current possible moves
 class Knight
-  def initialize(game, start)
+  def initialize(game, start, board)
     @game = game
     @start = start
+    @board = board
   end
 
   def rec(row, col)
-  steps = [
-        [-2, -1], # upper left 1
-        [-2, +1], # upper right 1
+    # binding.pry
+  steps_row = [
+        -2, 
+        -2, 
+        -1, 
+        -1, 
+        +2, 
+        +2, 
+        +1, 
+        +1 
+      ]
 
-        [-1, -2], # upper left 2
-        [-1, +2], # upper right 2
-
-        [+2, -1], # lower left 1
-        [+2, +1], # lower right 1
-
-        [+1, -2], # lower left 2
-        [+1, +2], # lower right 2
+      steps_col = [
+        -1,
+        +1,
+        -2,
+        +2,
+        -1,
+        +1,
+        -2,
+        +2
       ]
       i = 0
-      for [rowx, colx] in steps
+      new_row = nil
+      new_col = nil
+      # i have my start square and i can probably make a tree of possible moves from that square and
+      # then a tree of possible moves from it's children
+      for rowx in steps_row
+        # binding.pry
         new_row = rowx + row
-        new_col = colx + col
-        possible_moves(new_col, new_row)
+        
+        new_col = steps_col[i] + col
+         
+        @game.possible_moves(new_row, new_col) if (0..7).include?(new_row) && (0..7).include?(new_col)
+          
         i += 1
       end
 
-  end
-
-  def possible_moves(row, col)
 
   end
+
+ 
 
 end
 board = Board.new
 
 board.display_board
 
-board.knight_moves([3, 3], [1, 2])
+board.knight_moves([0, 0], [7, 7])
+
+p board.array
